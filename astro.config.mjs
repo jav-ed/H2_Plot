5c882ab2d503dbef3ca9b1dd7b8d2ebcde71718e
jav <Javed.butt@hotmail.de>
2023-07-02 18:24:38 +0200

    added deployment file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://jav-ed.github.io",
+  base: "paper_H2O_Webpage_Code",
   integrations: [
     tailwind(),
     image({
